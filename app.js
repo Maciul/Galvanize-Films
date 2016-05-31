79fb9ddc7aa514a0c7ae9dbad77ba8d5cfa3332e
Pawel <Pawel.Maciulewski@gmail.com>
2016-05-31 17:17:13 -0600

    include landing page and add.html

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-});
+
+    $('#myForm').on('submit', function(event) {
+    event.preventDefault();
+    $.post('https://mighty-eyrie-15280.herokuapp.com/films', $(this).serialize());
+    });
+  });
